c93d9004c915a47567ae9a3ad022f6999d256230
Shane Rogers <rogerssh@tcd.ie>
2013-06-05 09:52:43 -0600

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socrada2::Application.config.secret_token = 'a428f03e1d8f1ca90734b2600399d833cce45ac58987295342e5e4756b4f8148dba59ec1e0d0f81225f7d9f17a16606a06987d66a98ef43a9f96b8a437831500'
