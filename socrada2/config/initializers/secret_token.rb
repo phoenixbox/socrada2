a83886500bedf3b95ea17e191827a4dfcc64185d
Shane Rogers <rogerssh@tcd.ie>
2013-06-05 09:57:02 -0600

    delete duplicate

--- socrada2/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Socrada2::Application.config.secret_token = 'd430a5898b22181984433d12f2e3f28cf59f2663144f1fbd7277759e136e555b21e8b8e8b135f737b5f9dccd2c57a1a583b4b3d8ac4f837a2824f4398b8fe278'
